d2fbde63354eebdacfbbbf671a5343558a821580
zakaria touzri <zakadmaster@gmail.com>
2020-10-02 22:14:33 +0200

    wip

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function toggleShare() {
-    document.getElementById("share-button").classList.toggle("active");
+function closeShare() {
+  document.getElementById("share-button").classList.remove("active");
 }
+
+document.getElementById("share-button").addEventListener("click", function (e) {
+  e.stopPropagation();
+  document.getElementById("share-button").classList.toggle("active");
+});
