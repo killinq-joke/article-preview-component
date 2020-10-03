c7cb0a3ec69a421e053b95c8031fb9692acda9f7
zakaria touzri <zakadmaster@gmail.com>
2020-10-03 22:25:36 +0200

    ADDED: share display logic

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 function closeShare() {
-  document.getElementById("share-button").classList.remove("active");
+  //document.getElementById("share-button").classList.remove("active");
 }
+
+document.body.addEventListener("click", function (e) {
+  document.getElementById("share-button").classList.remove("active");
+  document.getElementById("pop-up").style.display = 'none';
+}, false);
 
 document.getElementById("share-button").addEventListener("click", function (e) {
   e.stopPropagation();
-  document.getElementById("share-button").classList.toggle("active");
-});
+  document.getElementById("share-button").classList.add("active");
+  document.getElementById("pop-up").style.display = 'flex';
+}, true);
