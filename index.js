51b20ce70ae7eae42eb4ca4544c6cf08d893fb5a
zakaria touzri <zakadmaster@gmail.com>
2020-10-03 23:17:35 +0200

    wip logic

--- index.js
+++ index.js
@@ -2,13 +2,32 @@ function closeShare() {
   //document.getElementById("share-button").classList.remove("active");
 }
 
-document.body.addEventListener("click", function (e) {
-  document.getElementById("share-button").classList.remove("active");
-  document.getElementById("pop-up").style.display = 'none';
-}, false);
+var shareActive = false;
 
-document.getElementById("share-button").addEventListener("click", function (e) {
-  e.stopPropagation();
-  document.getElementById("share-button").classList.add("active");
-  document.getElementById("pop-up").style.display = 'flex';
-}, true);
+document.body.addEventListener(
+  "click",
+  function (e) {
+    document.getElementById("share-button").classList.remove("active");
+    document.getElementById("pop-up").style.display = "none";
+  },
+  false
+);
+
+document.getElementById("share-button").addEventListener(
+  "click",
+  function (e) {
+    e.stopPropagation();
+    const shareButton = document.getElementById("share-button");
+    shareButton.classList.toggle("active");
+    if (shareButton.classList.contains("active")) {
+      document.getElementById("pop-up").style.display = "flex";
+    } else {
+      document.getElementById("pop-up").style.display = "none";
+    }
+  },
+  true
+);
+
+function log() {
+  console.log("slut");
+}
